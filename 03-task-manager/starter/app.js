b75d61099fee87ec8fd31300db2530277837c043
Greg Hayes <gthelight@gmail.com>
2022-03-30 18:50:25 -0400

    Initial commit

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,11 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+app.get("/hello", (req, res) => {
+  res.status(200).send("Task Manager App");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`server is runnig on port ${port}...`);
+});
