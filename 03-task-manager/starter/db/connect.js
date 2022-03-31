6ab5ed3db65375a6d23d33a667522485a36e76a5
Greg Hayes <ghayes@superduperinc.com>
2022-03-31 10:23:24 -0400

    Removes comments

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,9 +1,5 @@
 const mongoose = require("mongoose");
 
-// before making environment variable
-// const connectionString =
-//   "mongodb+srv://ghayes:j6sMttc4LEPHch@nodeprojects.xkcjg.mongodb.net/NodeProjects?retryWrites=true&w=majority";
-
 const connectDB = (url) => {
   return mongoose.connect(url, {
     useNewUrlParser: true,
