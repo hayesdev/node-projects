032b8883b142ec49f63332d8b45aa2d0964d7492
Greg Hayes <ghayes@superduperinc.com>
2022-04-19 16:02:18 -0400

    Adds product model and schema

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
-// async errors
+require("express-async-errors");
 
 const express = require("express");
 const app = express();
 
 const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
 
 const notFound = require("./middleware/not-found");
 const errorHandler = require("./middleware/error-handler");
@@ -17,7 +18,7 @@ app.get("/", (req, res) => {
   res.send("<h1>Store API</h1><a href='/api/v1/products'>Products route</a>");
 });
 
-// app.use("/api/v1/products");
+app.use("/api/v1/products", productsRouter);
 
 // products route
 app.use(notFound);
--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,7 +1,11 @@
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "products testing route " });
+  // instead of setting up custom middleware
+  throw new Error("testing async errors");
+  // res.status(200).json({ msg: "products testing route " });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "" });
+  res.status(200).json({ msg: "products route" });
 };
+
+module.exports = { getAllProducts, getAllProductsStatic };
