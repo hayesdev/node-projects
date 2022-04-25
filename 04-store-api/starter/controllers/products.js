6a48312ef5502749086cd550ca80a3021eb34970
Greg Hayes <ghayes@superduperinc.com>
2022-04-25 10:21:57 -0400

    Adds product controller functionality

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,11 +1,29 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
   // instead of setting up custom middleware
-  throw new Error("testing async errors");
-  // res.status(200).json({ msg: "products testing route " });
+  const products = await Product.find({
+    name: "high-back bench",
+  });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  const { featured, company } = req.query;
+  const queryObject = {};
+
+  // if featured does not exist, queryObject stays empty and
+  // find() returns all products
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+
+  // since req.query is an object we can pass it into find()
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = { getAllProducts, getAllProductsStatic };
