479d387436bcd21b847350996dbebc60da0c372f
Greg Hayes <ghayes@superduperinc.com>
2022-05-04 16:50:51 -0400

    Sets up basic auth

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,4 +1,19 @@
+//check username, password in post(login) request
+// if it exists create new JWT
+// send back to front-end
+// set up auth so only request with JWT can access dashboard
+
+const CustomAPIError = require("../errors/custom-error");
+
 const login = (req, res) => {
+  const { username, password } = req.body;
+  // mongo
+  // Joi
+  // check in controller
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide email and password", 400);
+  }
+  console.log(username, password);
   res.send("Fake Login/Register/Signup Route");
 };
 const dashboard = (req, res) => {
